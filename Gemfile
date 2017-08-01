7d792cc6dc3ca27bb63cb6fa4a980cbdd8e9a0fe
Marcelo Odir <marceloodir@gmail.com>
2017-07-31 21:22:42 -0300

    Criação de seeds para testes futuros...

--- Gemfile
+++ Gemfile
@@ -8,6 +8,10 @@ end
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.2'
+
+gem 'faker'
+gem 'cpf_utils'
+
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Gerando os Tipos de usuários...'
+Kind.create!([{description: "Palestrante"}, 
+	         {description: "Participante"},
+	         {description: "Avaliador"}])
+
+puts 'Gerando os Usuários...'
+10.times do |i|
+	User.create!(
+		name:  Faker::Name.name,
+		email: Faker::Internet.email,
+		cpf:   CpfUtils.cpf_formatted,
+		kind: Kind.all.sample)
+end
+
+
+
+
